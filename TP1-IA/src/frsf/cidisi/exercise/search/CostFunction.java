6500f502579b80c4e7ee6b2fc4a82e28435464d9
PabloBonet <pablo94_88@hotmail.com>
2015-05-22 05:33:04 -0300

    Funcion de costo
    Ver si la vamos a dejar definida asi

--- TP1-IA/src/frsf/cidisi/exercise/search/CostFunction.java
+++ TP1-IA/src/frsf/cidisi/exercise/search/CostFunction.java
@@ -1,8 +1,10 @@
 package frsf.cidisi.exercise.search;
 
 
+
 import frsf.cidisi.faia.solver.search.IStepCostFunction;
 import frsf.cidisi.faia.solver.search.NTree;
+import frsf.ia.tp.libreriaclases.FuncionesAuxiliares;
 
 /**
  * This class can be used in any search strategy like
@@ -17,8 +19,20 @@ public class CostFunction implements IStepCostFunction {
     public double calculateCost(NTree node) {
         
         //TODO: Complete Method
-    	
-        
+    	NTree padre=node.getParent();
+	   	StateDrone estadoPadre;
+	    StateDrone estadoActual=(StateDrone)node.getAgentState();
+	   
+	   	if(padre==null){
+	   		  return 0;
+	   		}
+	   	else
+	   		{
+	   		/*Retornaria 2 si no hay personas en la posicionActual
+	   		 * Retornaria 1 si hay personas en la posicion actual
+	   		 * */
+	   		}
+	   		
         return 0;
     }
 }
